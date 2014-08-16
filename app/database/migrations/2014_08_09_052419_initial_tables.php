8c59fc712649f05ebb3f35fb80141331b6499322
Duilio <silence@dessigual.com>
2014-08-16 14:01:02 -0430

    first commit, basic tables migrations

---
+++ app/database/migrations/2014_08_09_052419_initial_tables.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class InitialTables extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('sections', function (Blueprint $table) {
+            $table->increments('id');
+
+            //string -> varchar(255)
+            $table->string('name'); // Our company
+            $table->string('slug_url'); //our-company - company
+            $table->boolean('menu')->default(true);
+            $table->tinyInteger('menu_order')->unsigned()->default(200);
+            $table->string('type'); //page, blog, products, gallery
+
+            $table->boolean('published')->default(false);
+
+            $table->timestamps(); //created_at, update_at
+            $table->timestamp('published_at');
+            $table->softDeletes(); //deleted_at
+        });
+
+        Schema::create('pages', function (Blueprint $table) {
+            $table->increments('id');
+
+            // table sections -> FK section_id
+            $table->integer('section_id')->unsigned()->nullable();
+            $table->foreign('section_id')->references('id')->on('sections');
+
+            $table->string('title'); // The team
+            $table->string('slug_url'); //the-team - team
+            $table->tinyInteger('order_num')->unsigned()->default(200);
+            $table->text('body'); //content
+            $table->string('tab_title');
+            $table->mediumText('meta_description')->nullable();
+
+            $table->boolean('published')->default(false);
+            $table->boolean('featured')->default(false);
+
+            $table->timestamps(); //created_at, update_at
+            $table->timestamp('published_at');
+            $table->softDeletes(); //deleted_at
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::drop('sections');
+        Schema::drop('pages');
+	}
+
+}
